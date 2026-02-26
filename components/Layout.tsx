2076a9da56b472642a7dd3e723065752e1cd2e3c
Ruben Garcalia <rubeneitor2@hotmail.com>
2026-02-26 16:35:15 +0100

    TODO: Permitirlo en nginx para testear sin el proxy de code-server

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,12 +6,12 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
             <head>
                 <meta charSet="UTF-8" />
                 <title>Kolsor</title>
-                <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png"></link>
-                <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png"></link>
-                <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png"></link>
-                <link rel="shortcut icon" href="favicon/favicon.ico" type="image/x-icon" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"></link>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"></link>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"></link>
+                <link rel="shortcut icon" href="favicon/favicon.ico" type="/image/x-icon" />
                 {/* <link rel="manifest" href="/site.webmanifest"></link> */}
-                <link rel="stylesheet" href="style.css" />
+                <link rel="stylesheet" href="/style.css" />
             </head>
             <body>
                 <div id="root">{children}</div>
