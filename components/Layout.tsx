2d9389d48fcfe5dd005212970594aad147d068cc
Ruben Garcalia <rubeneitor2@hotmail.com>
2026-02-26 15:35:08 +0100

    Carga de css Quiza deberia hacer que se cargara desde url cuando tenga public funcionando

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,25 @@
 import React from "react"
+import { readFileSync } from "node:fs";
 
-export const Layout = ({ children }: { children: React.ReactNode }) => (
-    <html lang="es">
-        <head>
-            <meta charSet="UTF-8" />
-            <title>Kolsor</title>
-        </head>
-        <body>
-            <div id="root">{children}</div>
-        </body>
-    </html>
-);
+export const Layout = ({ children }: { children: React.ReactNode }) => {
+    let css = "";
+
+    try {
+        css = readFileSync("public/style.css").toString()
+    } catch { }
+
+    return (
+        <html lang="es">
+            <head>
+                <meta charSet="UTF-8" />
+                <title>Kolsor</title>
+                <style>
+                    {css}
+                </style>
+            </head>
+            <body>
+                <div id="root">{children}</div>
+            </body>
+        </html>
+    )
+};
