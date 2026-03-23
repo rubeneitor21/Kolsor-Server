6b3c27796fc885ccf69a59707d4e86213c685c1c
Ruben Garcalia <92950852+rubeneitor21@users.noreply.github.com>
2026-03-23 16:55:26 +0100

    Guay

--- public/game/main.js
+++ public/game/main.js
@@ -63,8 +63,7 @@
 
       if (data.type === "game-rolls") {
         let rollsDiv = document.querySelector("#rolls")
-        console.log(JSON.stringify(data.body[userId]))
-        rollsDiv.innerHTML = JSON.stringify(data.body[userId])
+        rollsDiv.innerHTML = "Tiradas Test:<br>" + JSON.stringify(data.body[userId], null, 2)
       }
     }
 
